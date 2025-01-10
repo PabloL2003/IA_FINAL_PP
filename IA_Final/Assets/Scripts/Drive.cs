9d576948f022fefba80c6062cba0b2024dd91bbb
pcelaya <125652831+pcelaya@users.noreply.github.com>
2025-01-10 19:35:03 +0100

    Tractor now Drives

--- IA_Final/Assets/Scripts/Drive.cs
+++ IA_Final/Assets/Scripts/Drive.cs
@@ -7,24 +7,25 @@ public class Drive : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public float currentSpeed = 0;
+    public Camera camera;
 
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        // Initialize translation and rotation
+        float translation = 0f;
+        float rotation = 0f;
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+        // Check for WASD inputs
+        if (Input.GetKey(KeyCode.W))    translation += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))    translation -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))    rotation -= rotationSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))    rotation += rotationSpeed * Time.deltaTime;
 
-        // Move translation along the object's z-axis
+        // Apply movement
         transform.Translate(0, 0, translation);
         currentSpeed = translation;
 
-        // Rotate around our y-axis
+        // Apply rotation
         transform.Rotate(0, rotation, 0);
     }
 }
